68dc51972c895801381e3e3d701293ea55acc812
Miguel Ángel Rodríguez <miguel.rodriguez@grupo2000.es>
2019-11-14 13:25:25 +0100

    Clarification of the purpose of this file

--- api_consultar_cliente_nif.php
+++ api_consultar_cliente_nif.php
@@ -1,4 +1,9 @@
 <?php
+
+/*
+	Ejemplo de cómo obtener el valor de id_show dado el cif/nif
+*/
+
     include 'Firmafy.php';
     $api = new firmafy();
 
@@ -6,7 +11,9 @@
         'action'  => 'Consultar_Cliente_Nif',
         'cif' => '99999999', // cif / nif
     );
-    echo $new_plan = $api -> setData($data) -> send();
+    $new_plan = $api -> setData($data) -> send();
     $new_plan = json_decode($new_plan);
-   
+    $id_show = $new_plan->data->id_show;
+
+    echo $id_show;
 ?>
